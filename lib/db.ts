b081fa6ae0ef14e96dd7bf8d3c0fae88ed30a467
furqan137 <149899415+furqan137@users.noreply.github.com>
2025-05-21 05:21:09 -0700

    Update dbupdate

--- lib/db.ts
+++ lib/db.ts
@@ -1,48 +1,12 @@
 import mongoose, { Mongoose } from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI;
-
-if (!MONGODB_URI) {
-  throw new Error(
-    'Please define the MONGODB_URI environment variable inside .env.local'
-  );
-}
-
-// Extend global object type for caching
-declare global {
-  // allow global `var` declarations
-  // eslint-disable-next-line no-var
-  var _mongoose: {
-    conn: Mongoose | null;
-    promise: Promise<Mongoose> | null;
-  } | undefined;
-}
-
-let cached = global._mongoose;
-
-if (!cached) {
-  cached = global._mongoose = { conn: null, promise: null };
-}
-
-async function dbConnect(): Promise<Mongoose> {
-  if (cached.conn) return cached.conn;
-
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
-
-    cached.promise = mongoose.connect(MONGODB_URI, opts);
+export async function dbConnect(): Promise<Mongoose> {
+  const MONGODB_URI = process.env.MONGODB_URI;
+  if (!MONGODB_URI) {
+    throw new Error("Please define the MONGODB_URI environment variable");
   }
 
-  try {
-    cached.conn = await cached.promise;
-  } catch (err) {
-    cached.promise = null;
-    throw err;
-  }
-
-  return cached.conn;
+  return mongoose.connect(MONGODB_URI, {
+    bufferCommands: false,
+  });
 }
-
-export default dbConnect;
